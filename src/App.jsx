53b9c2475ddab71d8996c4cb5578eb72d613d6b5
mhistiak3 <mhistiak2@gmail.com>
2025-05-20 22:25:54 +0600

    event stopPropagation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-function App() {
- 
-  function handleEvent(e){
-console.log(e.target);
+import Toolbar from "./components/Event";
 
+function App() {
+  function handleEvent(e) {
+    if (e.target.matches("li")) {
+      console.log(e.target);
+    }
   }
   return (
     <main className="app" style={{ padding: "20px" }}>
-
       <ul onClick={handleEvent}>
         <li>a</li>
         <li>b</li>
         <li>c</li>
       </ul>
+      <Toolbar />
+
+      <div
+        onClickCapture={() => {
+         console.log('s');
+         
+        }}
+      >
+        <button onClick={(e) => e.stopPropagation()} >sad</button>
+        <button onClick={(e) => e.stopPropagation()} >sad</button>
+      </div>
     </main>
   );
 }
---
+++ src/components/Event.jsx
@@ -0,0 +1,28 @@
+function Button({ onClick, children }) {
+  return (
+    <button onClick={e => {
+        console.dir(e);
+        
+      e.stopPropagation();
+      onClick();
+    }}>
+      {children}
+    </button>
+  );
+}
+
+export default function Toolbar() {
+  return (
+    <div className="Toolbar" onClick={() => {
+      alert('You clicked on the toolbar!');
+    }}>
+        dsc
+      <Button onClick={() => alert('Playing!')}>
+        Play Movie
+      </Button>
+      <Button onClick={() => alert('Uploading!')}>
+        Upload Image
+      </Button>
+    </div>
+  );
+}
